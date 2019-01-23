fa7576027020274560c36bb589f797e8a63682cf
Omer Dagan <omerd@riscogroup.com>
2019-01-23 22:32:40 +0200

    protocol mqtt
    Signed-off-by: Omer Dagan <omerd@riscogroup.com>

--- irigation_main/wifi.ino
+++ irigation_main/wifi.ino
@@ -51,15 +51,39 @@ void send_msg(char *msg) {
 }
 
 void mqtt_callback(char* topic, byte* payload, unsigned int length) {
-  String sTopic = String(topic);
+  //String sTopic = String(topic);
+  char * msg = (char *)malloc((length + 1) * sizeof(char));//String((char *) payload);
+  strncpy(msg, (char *)payload, length);
+  msg[length] = '\0';
+  char *p, *i;
+  
   Serial.print("Message arrived [");
-  Serial.print(sTopic);
-  Serial.print("] ");
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
+  Serial.print(topic);
+  Serial.print(" , ");
+  Serial.print(msg);
+  Serial.println("]");
+  
+  if (strstr(topic, "STATUS")) {
+    Serial.println("recived a STATUS message");
+    // call status handlin
+  } else if (strstr(topic, "SET")) {
+    Serial.print("recived a SET message - param: '");
+    p = strtok_r(msg," ",&i);
+    while (p) {
+      Serial.print(p);
+      Serial.print(" , ");
+      p = strtok_r(NULL," ",&i);
+    }
+    Serial.println("'");
+  } else if (strstr(topic, "GET")) {
+    Serial.println("recived a GET message");
+    do {
+      p = strtok_r(msg," ",&i);
+      Serial.print(p);
+      Serial.print(" ");
+    } while (p);
   }
-  Serial.println();
-
+  free(msg);
   /*
   // Switch on the LED if an 1 was received as first character
   if ((char)payload[0] == '1') {
